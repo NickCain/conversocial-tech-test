c5280461d02234187c1607f4f55dcebc32a71724
Nicholas Cain <nicholas.cain@blupixl.co.uk>
2017-06-07 09:18:58 +0100

    Create Header Component and import to the main App.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <div className="App-header">
+          <h2>An Awesome Chat App</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
