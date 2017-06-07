7129a92b79048ae3cf6a7f3457a7408ece5c0d66
Nicholas Cain <nicholas.cain@blupixl.co.uk>
2017-06-07 09:31:05 +0100

    Create selected chat component

---
+++ src/components/SelectedChat/SelectedChat.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './SelectedChat.css';
+
+class SelectedChat extends Component {
+  render() {
+    return (
+      <div className="Selected-chat">
+        <h3>Selected Chat</h3>
+      </div>
+    );
+  }
+}
+
+export default SelectedChat;
